11ee7f5a1a7956e482e2c9cbad98faf4b6552e6b
abuindario <darioabuin@outlook.com>
2021-12-13 15:32:59 +0100

    cambios varios para pulir el código

--- itog1-frontend/src/app/shared/app.menu.config.ts
+++ itog1-frontend/src/app/shared/app.menu.config.ts
@@ -2,8 +2,13 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
-  { id: 'employees', name: 'EMPLOYEES', icon: 'business_center', route: '/main/employees'},
-  { id: 'projects', name: 'PROJECTS', icon: 'people', route: '/main/projects'},
-  { id: 'employeesProjects', name: 'EMPLOYEESPROJECTS', icon: 'account_balance', route: '/main/employeesProjects'},
+  {
+    id: 'modules', name: 'MODULES', icon: 'remove_red_eye', opened: true,
+    items: [
+      { id: 'employees', name: 'EMPLOYEES', icon: 'business_center', route: '/main/employees'},
+      { id: 'projects', name: 'PROJECTS', icon: 'people', route: '/main/projects'},
+      { id: 'employeesProjects', name: 'EMPLOYEESPROJECTS', icon: 'account_balance', route: '/main/employeesProjects'},
+         ]
+  },
   { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
 ];
