cc49ed5b837ffa29919f568504626a9510af5f76
abuindario <darioabuin@outlook.com>
2021-12-17 13:08:32 +0100

    Modificado y completado servicio que limita el porcentaje de asignación al 100%

--- itog1-model/src/main/java/es/cd/dsnd/itog1/model/core/service/EmployeesService.java
+++ itog1-model/src/main/java/es/cd/dsnd/itog1/model/core/service/EmployeesService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import es.cd.dsnd.itog1.api.core.service.IEmployeesService;
 import es.cd.dsnd.itog1.model.core.dao.EmployeesDao;
 import es.cd.dsnd.itog1.model.core.dao.EmployeesProjectsDao;
+import es.cd.dsnd.itog1.model.core.dao.ProjectsDao;
 
 import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.common.dto.EntityResultMapImpl;
@@ -67,16 +68,15 @@ public class EmployeesService implements IEmployeesService {
 //			throws OntimizeJEERuntimeException {
 //	return this.daoHelper.insert(this.employeesProjectsDao, attrMap);
 //	}
+//	
 	@Override
 	public EntityResult employeeProjectInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
 			Map<String, Object> employee_id = new HashMap<String, Object>();
 			employee_id.put(EmployeesProjectsDao.ATTR_ID_EMPLOYEE, attrMap.get(EmployeesProjectsDao.ATTR_ID_EMPLOYEE));
 			List<String> projectList = new ArrayList<String>();
 			projectList.add(EmployeesProjectsDao.ATTR_ID_PROJECT);
-			projectList.add(EmployeesProjectsDao.ATTR_EP);
-			
-		  EntityResult employeeProjectQuery = this.employeeProjectQuery(employee_id, projectList);
-		  	  
+			projectList.add(EmployeesProjectsDao.ATTR_EP);			
+		  EntityResult employeeProjectQuery = this.employeeProjectQuery(employee_id, projectList);		  	  
 		  
 		  if (employeeProjectQuery.getCode() != EntityResult.OPERATION_WRONG && employeeProjectQuery.calculateRecordNumber() > 0) {
 			List<Integer> object = (List<Integer>) employeeProjectQuery.get(EmployeesProjectsDao.ATTR_EP);
@@ -84,7 +84,7 @@ public class EmployeesService implements IEmployeesService {
 			for(int actual : object) {
 				suma += actual;
 			}
-			int value = (int) attrMap.get(EmployeesProjectsDao.ATTR_EP);
+			int value = Integer.parseInt(attrMap.get(EmployeesProjectsDao.ATTR_EP).toString());
 			suma = suma + value;
 			if(suma <= 100) {
 				return this.daoHelper.insert(this.employeesProjectsDao, attrMap);
@@ -99,7 +99,15 @@ public class EmployeesService implements IEmployeesService {
 			  if(employeeProjectQuery.getCode() == EntityResult.OPERATION_WRONG) {
 				  return employeeProjectQuery;  
 			  }else {
-					return this.daoHelper.insert(this.employeesProjectsDao, attrMap);
+				  int value = Integer.parseInt(attrMap.get(EmployeesProjectsDao.ATTR_EP).toString());
+				  if (value <= 100) {
+					  return this.daoHelper.insert(this.employeesProjectsDao, attrMap);
+				  } else {
+					  EntityResult toret = new EntityResultMapImpl();
+					  toret.setCode(EntityResult.OPERATION_WRONG);
+					  toret.setMessage("La suma del % de implicación de un empleado no puede superar el 100%");
+					  return toret;
+					}
 			  }
 		  }  
 	}
